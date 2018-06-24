d519ecd2a0fb1948ef040657fd6d30ccaea9b018
Trigger-broom-289 <stephane@dslhome.org>
2018-06-24 23:09:38 +0200

    minor updates, clean up

--- indi-driver/odroid-indi-roof/odroidroof.cpp
+++ indi-driver/odroid-indi-roof/odroidroof.cpp
@@ -219,7 +219,6 @@ void OdroidRoof::TimerHit()
                DEBUG(INDI::Logger::DBG_WARNING, "Turning off relay. Setting GPIO0 -> 1 GPIO1 -> 1");
                system("gpio write 0 1");
                system("gpio write 1 1");
-               //calling setParked(false) here caauses the driver to crash with nothing logged (looks like possibly an issue writing parking data). Therefore the next 4 lines are doing what is done in indidome.cpp' function. We dont care about parking data anyway as we get the parked state directly from the roof stop-switches.
                IUResetSwitch(&ParkSP);
                ParkS[1].s = ISS_ON;
                ParkSP.s = IPS_OK;
